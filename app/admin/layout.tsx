d8323db8cc8dffdc71c2a6f8779d4c1d92fdd894
youreachedrahat <70068895+youreachedrahat@users.noreply.github.com>
2024-12-23 11:01:53 +0530

    layout for user & admin

---
+++ app/admin/layout.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import LoginModal from "@/components/LoginModal";
+import { useAuth } from "@/context/AuthProvider";
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+
+    const { currentUser, userDataPrivate, userDataProfile } = useAuth();
+
+    if (!currentUser) return (
+        <LoginModal />
+    )
+    if (!userDataPrivate && !userDataProfile) return (
+        <>Loading</>
+    )
+    return (
+        { children }
+    );
+}
